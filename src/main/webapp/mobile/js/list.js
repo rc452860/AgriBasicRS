de770b59a330aad5938c2ede48c54b1b9f0d84a3
Sakura <87719349@qq.com>
2016-04-27 22:47:12 +0800

    修改一下js的方式。觉得好多东西要重构。操蛋。蛋疼 fuck damn it what fuck the day.

--- src/main/webapp/mobile/js/list.js
+++ src/main/webapp/mobile/js/list.js
@@ -1,52 +1,57 @@
 /**
  * Created by rc452 on 2016/4/27.
  */
-function Delete(id) {/*删除脚本*/
-    if(confirm("你确定要删除吗？")) {
-        $.ajax({
-            cache: false,
-            type: "POST",
-            url: window.deleteUrl, /*此处需修改*/
-            data: {'id': id},
-            async: false,
-            error: function (XMLHttpRequest) {
-                alert("error:" + XMLHttpRequest.status);
-            },
-            success: function (data) {
-                if (data.message != null) alert(data.message);
-                /*if(data.url != null) window.location.href = data.url;*/
-                if (data.message == "删除成功")
-                    window.location.reload();
-            }
-        });
-    }
-}
-function DeleteMulit() {
-    var arr = new Array();
-    $(".ctr:checked").each(function () {
-        arr.push($(this).val())
-    });
-    if(confirm("你确定要删除吗？")) {
-        $.ajax({
-            cache: false,
-            type: "POST",
-            url: window.deleteMulUrl, /*此处需修改*/
-            data: {ids: arr},
-            async: false,
-            error: function (XMLHttpRequest) {
-                alert("error:" + XMLHttpRequest.status);
-            },
-            success: function (data) {
-                if (data.message != null) alert(data.message);
-                /*if(data.url != null) window.location.href = data.url;*/
-                if (data.message == "删除成功")
-                    window.location.reload();
-            }
-        });
-    }
-}
+
 $(function () {
     $("#ctrall").click(function () {
         $(".ctr").attr("checked", $(this).attr("checked") != null);
     })
+    window.Delete = function(id) {/*删除脚本*/
+        if(confirm("你确定要删除吗？")) {
+            $.ajax({
+                cache: false,
+                type: "POST",
+                url: window.deleteUrl, /*此处需修改*/
+                data: {'id': id},
+                async: false,
+                error: function (XMLHttpRequest) {
+                    alert("error:" + XMLHttpRequest.status);
+                },
+                success: function (data) {
+                    if (data.message != null) alert(data.message);
+                    /*if(data.url != null) window.location.href = data.url;*/
+                    if (data.message == "删除成功")
+                        window.location.reload();
+                }
+            });
+        }
+    }
+    window.DeleteMulit = function() {
+        var arr = new Array();
+        $(".ctr:checked").each(function () {
+            arr.push($(this).val())
+        });
+        if(confirm("你确定要删除吗？")) {
+            $.ajax({
+                cache: false,
+                type: "POST",
+                url: window.deleteMulUrl, /*此处需修改*/
+                data: {ids: arr},
+                async: false,
+                error: function (XMLHttpRequest) {
+                    alert("error:" + XMLHttpRequest.status);
+                },
+                success: function (data) {
+                    if (data.message != null) alert(data.message);
+                    /*if(data.url != null) window.location.href = data.url;*/
+                    if (data.message == "删除成功")
+                        window.location.reload();
+                }
+            });
+        }
+    }
+
+    window.dialog = function(url){
+        //弹窗添加原型带写
+    }
 })
